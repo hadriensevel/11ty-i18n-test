d47a28eee9872ce94e5e05846a9fdff7f6090dc8
hadriensevel <hadrien.sevel@outlook.com>
2023-10-16 17:51:19 +0200

    Big bang 💥

---
+++ src/_data/i18n/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    hello: {
+        'en': 'Hello!',
+        'it': 'Ciao!'
+    },
+    title: {
+        'en': 'Internationalization test',
+        'it': 'Test di internazionalizzazione'
+    },
+    welcome: {
+        'en': 'Welcome to this website about trains!',
+        'it': 'Benvenuto su questo sito web sui treni!'
+    }
+}
